c45e10755a4a83eeec358f85c26c1934affa1d61
Adrian <adiq@adiq.eu>
2018-08-23 10:19:17 +0200

    Update 🐈 gifs

--- flask-app/app.py
+++ flask-app/app.py
@@ -5,18 +5,22 @@ app = Flask(__name__)
 
 # list of cat images
 images = [
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr01/15/9/anigif_enhanced-buzz-31540-1381844535-8.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26390-1381844163-18.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-1376-1381846217-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/9/anigif_enhanced-buzz-3391-1381844336-26.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-29111-1381845968-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/9/anigif_enhanced-buzz-3409-1381844582-13.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr02/15/9/anigif_enhanced-buzz-19667-1381844937-10.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26358-1381845043-13.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-18774-1381844645-6.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-25158-1381844793-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/10/anigif_enhanced-buzz-11980-1381846269-1.gif"
+    "https://media.giphy.com/media/11c7UUfN4eoHF6/giphy.gif",
+    "https://media.giphy.com/media/eDgmbiQcujjsA/giphy.gif",
+    "https://media.giphy.com/media/Ov5NiLVXT8JEc/giphy.gif",
+    "https://media.giphy.com/media/NqZn5kPN8VVrW/giphy.gif",
+    "https://media.giphy.com/media/B6odR0DhsStfW/giphy.gif",
+    "https://media.giphy.com/media/B6odR0DhsStfW/giphy.gif",
+    "https://media.giphy.com/media/13borq7Zo2kulO/giphy.gif",
+    "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif",
+    "https://media.giphy.com/media/3oEduSbSGpGaRX2Vri/giphy.gif",
+    "https://media.giphy.com/media/3oEduSbSGpGaRX2Vri/giphy.gif",
+    "https://media.giphy.com/media/tBxyh2hbwMiqc/giphy.gif",
+    "https://media.giphy.com/media/Hcw7rjsIsHcmk/giphy.gif",
+    "https://media.giphy.com/media/GtZzfs0KuEjQY/giphy.gif",
+    "https://media.giphy.com/media/L76sH9DjvVyik/giphy.gif",
+    "https://media.giphy.com/media/MpSgICvhxS2kw/giphy.gif",
+    "https://media.giphy.com/media/hXafx5jj9HVBe/giphy.gif"
 ]
 
 @app.route('/')
